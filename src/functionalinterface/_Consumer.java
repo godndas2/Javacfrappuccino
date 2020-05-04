c5543a254e2f50babf8f891bfc5942c06d1cfd7c
halfdev <gusgodnd@naver.com>
2020-05-04 14:10:26 +0900

    _consumer class Done

--- src/functionalinterface/_Consumer.java
+++ src/functionalinterface/_Consumer.java
@@ -1,17 +1,39 @@
 package functionalinterface;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public class _Consumer {
     public static void main(String[] args) {
         // Normal Java Function
         Customer halfdev = new Customer("HALFDEV", "gusgodnd@naver.com");
         greetCustomer(halfdev);
-        // TODO greetCustomerV2
+        greetCustomerV2(halfdev, false);
+        // Consumer Functional interface
+        greetCustomerConsumer.accept(halfdev);
+        greetCustomerConsumerV2.accept(halfdev, false);
     }
 
+    // Normal Java Functional
     static void greetCustomer(Customer customer) {
         System.out.println("Hello, " + customer.getName() + ", thanks for registering Email " +
                 customer.getEmail());
     }
+    static void greetCustomerV2(Customer customer, boolean showEmail) {
+        System.out.println("Hello, " + customer.getName() + ", thanks for registering Email " +
+                (showEmail ? customer.getEmail() : "*********"));
+    }
+
+    // Consumer Functional
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello ~ " + customer.getName() + ", thanks for registering Email " +
+                    customer.getEmail());
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = ((customer, showEmail) ->
+            System.out.println("Hello ~ " + customer.getName() + ", thanks for registering Email " +
+                    (showEmail ? customer.getEmail() : "*********")));
+
 
     // for _Consumer static Class
     static class Customer {
