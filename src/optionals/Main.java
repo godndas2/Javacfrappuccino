f24ab31d4e4a64dd4bbe4754fbb34ba06a9ee9e7
halfdev <gusgodnd@naver.com>
2020-05-05 01:16:26 +0900

    Optional

---
+++ src/optionals/Main.java
@@ -0,0 +1,10 @@
+package optionals;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        Optional.ofNullable("able NULL")
+                .ifPresent(test -> System.out.println("PASS The Test " + test));
+    }
+}
