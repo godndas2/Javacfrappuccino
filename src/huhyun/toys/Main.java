31528ef0c107102a47571a4e7449a1b1c2a3bd81
halfdev <gusgodnd@naver.com>
2020-05-07 23:50:34 +0900

    toys

--- src/huhyun/toys/Main.java
+++ src/huhyun/toys/Main.java
@@ -3,36 +3,42 @@ package huhyun.toys;
 import huhyun.toys.Toy.Lego;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static huhyun.toys.ToyRegistrationValidator.isAgeValid;
-import static huhyun.toys.ToyRegistrationValidator.isManufacturingValidPredicate;
+import static huhyun.toys.ToyRegistrationValidator.*;
 import static huhyun.toys.ValidResultToy.SUCCESS;
 
 public class Main {
     public static void main(String[] args) {
-        Toy toy = new Toy(1L, "yellow", 10.5,
-                new Lego("KING LEGO",
-                        LocalDate.of(2020,5,6),
-                        LocalDate.of(1993,11,23)));
+        Toy toy = new Toy(1L, "yellow", 10.5);
 
         Lego lego = new Lego("KING LEGO",
                 LocalDate.of(2020,5,6),
                 LocalDate.of(1993,11,23));
 
-        ValidResultToy resultToy = isAgeValid()
+        ValidResultToy resultToy =
+                isAgeValid()
                 .apply(lego);
 
         System.out.println("<<<< RESULT >>>>");
 
-        if (resultToy != SUCCESS) {
-            throw new IllegalStateException(resultToy.name() + "NOT VALID");
-        } else {
-            System.out.println(resultToy.name());
-            System.out.println(lego.getName());
-        }
+            if (resultToy != SUCCESS)
+                throw new IllegalStateException(lego.getName() + " IS " + resultToy.name());
+            else {
+                System.out.println(lego.getName() + " IS " + resultToy.name());
+            }
 
-        System.out.println("<<< PREDICATE >>>");
+        System.out.println("<<< PREDICATE RESULT >>>");
         System.out.println(isManufacturingValidPredicate.test(lego.getManufacturingYear())); // true
 
+        System.out.println("<<< SUPPLIER >>>");
+        List<String> list = getColors.get();
+        System.out.println(
+        list.stream()
+                .map(String::toLowerCase)
+//                .forEach(System.out::println)
+                .collect(Collectors.joining(" "))
+        );
     }
 }
--- src/huhyun/toys/ToyRegistrationValidator.java
+++ src/huhyun/toys/ToyRegistrationValidator.java
@@ -4,8 +4,11 @@ import huhyun.toys.Toy.Lego;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static huhyun.toys.ValidResultToy.AGE_NOT_VALIID;
 import static huhyun.toys.ValidResultToy.SUCCESS;
@@ -20,6 +23,13 @@ public interface ToyRegistrationValidator extends Function<Lego, ValidResultToy>
     static Predicate<LocalDate> isManufacturingValidPredicate = legoOfManufacturingYears ->
             legoOfManufacturingYears.getYear() >= 2020;
 
+    static Supplier<List<String>> getColors = ()
+            -> Arrays.asList(
+                    "RED",
+                    "BLACK",
+                    "GREEN",
+                    "YELLOW"
+    );
 }
 
 enum ValidResultToy {
