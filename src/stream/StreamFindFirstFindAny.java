ba48b826cff9e5042046949ae672377cbb86b99c
halfdev <gusgodnd@naver.com>
2020-05-24 15:57:58 +0900

    stream findfirst() findany()

---
+++ src/stream/StreamFindFirstFindAny.java
@@ -0,0 +1,38 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamFindFirstFindAny {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 2, 1);
+
+        Optional<Integer> first = integers.stream().findFirst();
+        if (first.isPresent()) {
+            Integer integer = first.get();
+            System.out.println(integer); // 1
+        } else {
+            System.out.println("No Value");
+        }
+
+        Optional<Integer> first1 = integers.stream().filter(x -> x > 1).findFirst();
+        if (first1.isPresent()) {
+            System.out.println(first1.get()); // 2
+        } else {
+            System.out.println("No Value");
+        }
+
+        List<String> strings = Arrays.asList("node", "java", "c", "python");
+        Optional<String> first2 = strings.stream()
+                .filter(x -> !x.equalsIgnoreCase("node")).findFirst();
+
+        if (first2.isPresent()) {
+            System.out.println(first2.get()); // java
+        } else {
+            System.out.println("No Value");
+        }
+
+        // TODO findAny()
+    }
+}
