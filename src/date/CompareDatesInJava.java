946c4dcc3f38a0cbc8491ecc0be43559b682164c
halfdev <gusgodnd@naver.com>
2020-06-04 23:59:43 +0900

    date

--- src/date/CompareDatesInJava.java
+++ src/date/CompareDatesInJava.java
@@ -2,6 +2,8 @@ package date;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class CompareDatesInJava {
@@ -31,7 +33,55 @@ public class CompareDatesInJava {
             System.out.println("How to get here?");
         }
 
+        System.out.println();
         // A more user friendly method to compare
+        if (date.after(date2)) {
+            System.out.println("Date is after Date2");
+        }
+
+        if (date.before(date2)) {
+            System.out.println("Date is before Date2");
+        }
+
+        if (date.equals(date2)) {
+            System.out.println("Date is equal Date2");
+        }
+
+        System.out.println();
+        // java8
+        // isBefore(), isAfter(), isEqual() and compareTo()
+        DateTimeFormatter sdf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.of(2020, 12, 31);
+        LocalDate localDate2 = LocalDate.of(2021,1, 31);
+
+        System.out.println("localDate : " + sdf.format(localDate));
+        System.out.println("localDate2 : " + sdf.format(localDate2));
+
+        System.out.println("Is...");
+
+        if (localDate.isAfter(localDate2)) {
+            System.out.println("LocalDate is after LocalDate2");
+        }
+
+        if (localDate.isBefore(localDate2)) {
+            System.out.println("LocalDate is before LocalDate2");
+        }
+
+        if (localDate.isEqual(localDate2)) {
+            System.out.println("LocalDate is equal LocalDate2");
+        }
+
+        System.out.println("CompareTo..");
+
+        if (localDate.compareTo(localDate2) > 0) {
+            System.out.println("LocalDate is after LocalDate2");
+        } else if (localDate.compareTo(localDate2) < 0) {
+            System.out.println("LocalDate is Before LocalDate2");
+        } else if (localDate.compareTo(localDate2) == 0) {
+            System.out.println("LocalDate is equal to LocalDate2");
+        } else {
+            System.out.println("How to get here?");
+        }
 
     }
 }
